804cde8dff434dcbde985d590c5b8660a0488281
gyurisc <gyurisc@gmail.com>
2011-08-11 16:00:11 +0200

    Adding an MVC project to the solution.

---
+++ GooglePlusStats/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GooglePlusStats.Models;
+using GooglePlus; 
+
+namespace GooglePlusStats.Controllers
+{
+    public class HomeController : Controller
+    {
+        GooglePlus.GooglePlusDb db = new GooglePlus.GooglePlusDb(Properties.Settings.Default.GooglePlusDB);
+
+        public ActionResult Index() 
+        {
+            ViewBag.Message = "Google+ Statistics";
+            List<Connection> connections = db.Connections.ToList();
+            List<Person> persons = db.Persons.ToList();
+
+            return View(persons);
+        }
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ GooglePlusStats/Models/GoolePlusDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using GooglePlus; 
+
+namespace GooglePlusStats.Models
+{
+    public class GooglePlusDB : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Connection> Connections { get; set; }
+    }
+}
