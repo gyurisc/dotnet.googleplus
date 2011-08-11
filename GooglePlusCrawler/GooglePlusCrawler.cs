9110ad63a484729fbbfbae897c0c48a792865a49
gyurisc <gyurisc@gmail.com>
2011-08-11 15:04:20 +0200

    Adding GooglePlusCrawler to the project

---
+++ GooglePlusCrawler/GooglePlusCrawler.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GooglePlus;
+using System.Data.Entity.Validation; 
+
+namespace GooglePlusCrawler
+{
+    public class GooglePlusCrawler
+    {
+        private List<string> visitedIDList = new List<string>();
+        private Queue<string> visitingIDList = new Queue<string>();
+        
+        private GooglePlusDb db; 
+
+        public GooglePlusCrawler(GooglePlusDb _db)
+        {
+            db = _db; 
+        }
+
+        /// <summary>
+        /// Crawls a person starting from the start id
+        /// </summary>
+        /// <param name="start_id"></param>
+        /// <param name="counter"></param>
+        public void CrawlPerson(string start_id, int numberOfPersons, bool checkConnections)
+        {
+            string current_id; 
+
+            GooglePlusService gpsvc = new GooglePlusService();
+
+            InitializeVisitLists();
+
+            if (visitingIDList.Count == 0)
+            {
+                // seed the queue with first id 
+                visitingIDList.Enqueue(start_id);                
+            }
+
+            while (numberOfPersons > 0)
+            {                
+                if (visitingIDList.Count == 0)
+                {
+                    break;
+                }
+
+                current_id = visitingIDList.Dequeue();
+
+                // did we see this user already
+                if (visitedIDList.Contains(current_id))
+                {
+                    continue;     
+                }
+
+                if (PersonInDb(current_id))
+                {
+                    if (!visitedIDList.Contains(current_id))
+                    {
+                        visitedIDList.Add(current_id);
+                    }      
+
+                    continue; 
+                }
+                else
+                {
+                    Person p = gpsvc.GetPerson(current_id);
+
+                    db.Persons.Add(p);
+                    db.SaveChanges();
+
+                    if (!visitedIDList.Contains(current_id))
+                    {
+                        visitedIDList.Add(current_id);
+                    }
+
+                    if (!checkConnections)
+                    {
+                        Console.Write(".");
+                        continue; 
+                    }
+
+                    List<Connection> followed = gpsvc.GetFollowedConnections(current_id);
+                    ProcessConnections(followed);                     
+
+                    List<Connection> followers = gpsvc.GetFollowerConnections(current_id);
+                    ProcessConnections(followers);            
+                }
+
+                numberOfPersons--;
+                Console.Write(".");
+            }
+        }
+
+        private void InitializeVisitLists()
+        {
+            List<Person> persons = db.Persons.ToList();
+
+            foreach (Person p in persons)
+            {
+                visitedIDList.Add(p.ID);
+            }
+
+            List<Connection> connections = db.Connections.ToList();
+
+            foreach (Connection connection in connections)
+            {
+                if (!visitedIDList.Contains(connection.ToID))
+                {
+                    visitingIDList.Enqueue(connection.ToID);
+                }
+            }
+        }
+
+        private void ProcessConnections(List<Connection> connections)
+        {
+            if (connections.Count < 1)
+            {
+                return; 
+            }
+
+            foreach (Connection con in connections)
+            {
+                string id = con.ToID;
+                RegisterID(id);
+
+                if (!ConnectionExists(con))
+                {
+                    db.Connections.Add(con);
+                    //try
+                    //{
+                        
+                        
+                    //}
+                    //catch (DbEntityValidationException validationException)
+                    //{
+                    //    Console.WriteLine("Saving Connection to db failed for the following reasons: ");
+
+                    //    foreach (DbEntityValidationResult validationResult in validationException.EntityValidationErrors)
+                    //    {
+                    //        foreach (DbValidationError error in validationResult.ValidationErrors)
+                    //        {
+                    //            Console.WriteLine("Error: " + error.ErrorMessage);
+                    //        }
+                    //    }
+                    //}
+                }                
+            }
+
+            db.SaveChanges();
+        }
+
+        private bool ConnectionExists(Connection con)
+        {
+            if (!visitingIDList.Contains(con.ToID))
+            {
+                return false; 
+            }
+
+            if (visitedIDList.Contains(con.FromID))
+            {
+                return true; 
+            }
+
+            if (visitingIDList.Contains(con.FromID))
+            {
+                return true; 
+            }
+
+            return false; 
+        }
+
+        /// <summary>
+        /// Registers an id for further crawling if needed 
+        /// </summary>
+        /// <param name="id"></param>
+        private void RegisterID(string id)
+        {
+            if (!visitedIDList.Contains(id))
+            {                    
+                visitingIDList.Enqueue(id);
+            }
+        }
+
+        private bool PersonInDb(string plus_id)
+        {
+            Person person = db.Persons.Find(plus_id);
+            return (person == null) ? false : true; 
+        }
+
+        private bool ConnectionInDb(Connection connection)
+        {
+            int count = (from c in db.Connections
+                         where (c.ToID == connection.ToID && c.FromID == connection.FromID)
+                         select c).ToList().Count;
+
+            return count == 0 ? false : true; 
+        }   
+    }
+}
---
+++ GooglePlusCrawler/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GooglePlus;
+
+namespace GooglePlusCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string seed_id = "116710411414066669240";            
+
+            // string connection_db = @"Data Source=C:\Users\Cris\dev\dotnet.googleplus\GooglePlusCrawler\Data\GooglePlus.sdf";
+            string connection_db = @"Data Source=t61\sqlexpress;Initial Catalog=GooglePlusStats.Models.GooglePlusDB;Integrated Security=True";
+            GooglePlusDb db = new GooglePlusDb(connection_db);
+            GooglePlusCrawler crawler = new GooglePlusCrawler(db);
+            crawler.CrawlPerson(seed_id, 50, true);
+        }
+    }
+}
